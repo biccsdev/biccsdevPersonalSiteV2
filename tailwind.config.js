c711282bd43910e54edc3313a9ecc54a8ac7409f
biccs <biccsdev@gmail.com>
2022-03-15 15:52:24 -0700

    fixed mobile screen ratio

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        'sm': '390px',
+        'sm': '320px',
         // => @media (min-width: 640px) { ... }
   
         'md': '768px',
